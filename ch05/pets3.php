62cea522f70732530b41b0962351b543ebb0eee8
Patrick Toy <patrickjtoy@gmail.com>
2014-04-28 14:26:16 -0400

    Completed chapter 5 tutorials

---
+++ ch05/pets3.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Pets</title>
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	<body>
+		<?php # Script 5.3 - pets2.php
+			// This page defines and uses the Pet, Cat, and Dog classes.
+
+			# ***** CLASSES ***** #
+
+			/* Class Pet.
+			 * The class contains one attribute: name.
+			 * The class contains three methods:
+			 * - __construct()
+			 * - eat()
+			 * - sleep()
+			 */
+			class Pet {
+
+				// Declare the attributes:
+				public $name;
+
+				// Constructor assigns the pet's name:
+				function __construct($pet_name) {
+					$this->name = $pet_name;
+					self::sleep();
+				}
+
+				// Pets can eat:
+				function eat() {
+					echo "<p>{$this->name} is eating.</p>";
+				}
+
+				// Pets can sleep:
+				function sleep() {
+					echo "<p>{$this->name} is sleeping.</p>";
+				}
+
+				// Pets can play:
+				function play() {
+					echo "<p>{$this->name} is playing.</p>";
+				}
+			} // End of Pet class.
+
+			/* Cat class extends Pet.
+			 * Cat overrides play().
+			 */
+			class Cat extends Pet {
+				function play() {
+
+					// Call the Pet::play() method:
+					parent::play();
+
+					echo "<p>{$this->name} is climbing.</p>";
+				}
+			} // End of Cat class
+
+			/* Dog class extends Pet.
+			 * Dog overrides play().
+			 */
+			class Dog extends Pet {
+				function play() {
+
+					// Call the Pet::play() method:
+					parent::play();
+
+					echo "<p>{$this->name} is fetching.</p>";
+				}
+			} // End of Dog class.
+
+			# ***** END OF CLASSES ***** #
+
+			// Create a dog:
+			$dog = new Dog('Satchel');
+
+			// Create a cat:
+			$cat = new Cat('Bucky');
+
+			// Create an unknown type of pet:
+			$pet = new Pet('Bob');
+
+			// Feed them:
+			$dog->eat();
+			$cat->eat();
+			$pet->eat();
+
+			// Nap time:
+			$dog->sleep();
+			$cat->sleep();
+			$pet->sleep();
+
+			// Do animal-specific thing:
+			$dog->play();
+			$cat->play();
+			$pet->play();
+
+			// Delete the objects:
+			unset($dog, $cat, $play);
+
+		?>
+	</body>
+</html>
