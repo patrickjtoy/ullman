353c5da66a05d8a84407d6a2a40910906fb51472
Patrick Toy <patrickjtoy@gmail.com>
2014-04-28 16:02:24 -0400

    Completed tutorials 6.1, 6.2, & 6.3

---
+++ ch06/class.shape.php
@@ -0,0 +1,20 @@
+<?php # Script 6.1 - class.shape.php
+
+	/* This page defines the Shape abstract class.
+	 * The class contains no attributes.
+	 * The class containes two abstract methods:
+	 * - getArea()
+	 * - getPerimeter()
+	 */
+	abstract class Shape {
+
+		// No attributes to declare.
+		// No constructor or destructor defined here.
+
+		// Method to calculate and return the area.
+		abstract protected function getArea();
+
+		// Method to calculate and return the perimeter.
+		abstract protected function getPerimeter();
+
+	} // End of Shape class.
